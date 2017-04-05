67468d5b0285ec915cbd64032fab5fab7915dc4d
Awaes Choudhary <acedu77@gmail.com>
2017-04-05 19:53:22 -0400

    added user authentication

--- src/main/java/com/codelot/controller/HomeController.java
+++ src/main/java/com/codelot/controller/HomeController.java
@@ -1,7 +1,11 @@
 package com.codelot.controller;
 
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by awaeschoudhary on 3/31/17.
@@ -10,7 +14,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
     @RequestMapping(value = "/")
-    public String home(){
-        return "WEB-INF/pages/home";
+    public ModelAndView home(){
+        UserService userService = UserServiceFactory.getUserService();
+        User user = userService.getCurrentUser();
+
+        //if there is no logged in user, redirect to homepage
+        if(user == null){
+            ModelAndView model = new ModelAndView("WEB-INF/pages/home");
+            return model;
+        }
+        else{
+            //redirect to map select page if there is a logged in user
+            ModelAndView model = new ModelAndView("WEB-INF/pages/MapSelect");
+            return model;
+        }
+    }
+
+    @RequestMapping(value = "/signup")
+    public ModelAndView signup(){
+        //redirect to google auth page. On signing up, user is redirected to profile page
+        UserService userService = UserServiceFactory.getUserService();
+        ModelAndView model = new ModelAndView("redirect:" + userService.createLoginURL("/profile"));
+        return model;
+    }
+
+    @RequestMapping(value = "/profile")
+    public String profile(){
+        return "WEB-INF/pages/Profile";
+    }
+
+    @RequestMapping(value = "/signin")
+    public ModelAndView signin(){
+        //redirect to google auth page. On signing in, user should be redirected to mapselect
+        UserService userService = UserServiceFactory.getUserService();
+        ModelAndView model = new ModelAndView("redirect:" + userService.createLoginURL("/"));
+        return model;
+    }
+
+    @RequestMapping("/signout")
+    public ModelAndView signout (){
+        UserService userService = UserServiceFactory.getUserService();
+        ModelAndView model = new ModelAndView("redirect:" + userService.createLogoutURL("/"));
+        return model;
     }
 }
--- src/main/webapp/WEB-INF/pages/home.jsp
+++ src/main/webapp/WEB-INF/pages/home.jsp
@@ -1,9 +1,12 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
     <head>
-
     </head>
     <body>
         <h2>Home Page</h2>
+        <form method="post">
+            <button title="Sign Up" type="submit" onclick="form.action='/signup';">Sign Up</button>
+            <button title="Sign In" type="submit" onclick="form.action='/signin';">Sign In</button>
+        </form>
     </body>
 </html>
